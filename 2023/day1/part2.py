d0134b50c1fd15c7896f4e8d3537db2179a6bb75
FySjutton <rubik.sten@gmail.com>
2023-12-01 12:56:52 +0100

    start on python solution

---
+++ 2023/day1/part2.py
@@ -0,0 +1,37 @@
+import os
+data = open(os.getcwd() + "/2023/day1/data.txt", "r")
+get_number = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+for x in data.readlines():
+    letter_str, number_str = "", ""
+    for y in x:
+        if y.isdigit():
+            letter_str += y
+        else:
+            number_str += y
+# const fs = require('fs');
+# const path = require('path');
+# var total = 0;
+
+# fs.readFile(path.join(__dirname, 'data.txt'), 'utf8', (err, data) => {
+#     for (x of data.split('\n')) {
+#         let letter_str = "";
+#         let nmb = "";
+#         const test = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+#         for (i of x) {
+#             if (isNaN(parseFloat(i))) {
+#                 letter_str += i;
+#             } else {
+#                 nmb += i;
+#             }
+#             for (p of Object.keys(test)) {
+#                 if (letter_str.includes(p) && letter_str != "") {
+#                     nmb += test[p];
+#                     letter_str = p[p.length - 1];
+#                 }
+#             }
+#         }
+#         total += parseInt(nmb[0] + nmb[nmb.length - 1]);
+#     }
+#     console.log(total)
+# });
